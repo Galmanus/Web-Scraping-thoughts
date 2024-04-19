bb07bfa2f7c7ce1640ed6a489cc0002b34a42fd2
galmanus <m.galmanus@gmail.com>
2024-04-19 12:59:09 -0300

    First basic example

--- web_0.py
+++ web_0.py
@@ -1,18 +1,18 @@
 # Import the necessary modules
-from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup as soup
+from urllib.request import Request, urlopen  # Importing modules for making HTTP requests and handling responses
+from bs4 import BeautifulSoup as soup  # Importing BeautifulSoup module for parsing HTML and XML documents
 
 # Set the URL of the webpage to scrape
-url = "https://www.example.com"
+url = "https://www.google.com"  # Defining the URL of the webpage to be scraped
 
 # Create a Request object with a custom User-Agent header
-req = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+req = Request(url, headers={"User-Agent": "Mozilla/5.0"})  # Creating a Request object with a custom User-Agent header to mimic a web browser
 
 # Send an HTTP request to the specified URL and read the response content
-webpage = urlopen(req).read()
+webpage = urlopen(req).read()  # Sending an HTTP request to the specified URL and reading the response content
 
 # Parse the HTML content of the webpage using the html.parser
-page_soup = soup(webpage, "html.parser")
+page_soup = soup(webpage, "html.parser")  # Parsing the HTML content of the webpage using the html.parser
 
 # Search for the first <title> tag in the HTML document and store it in a variable called title
-title = page_soup.find("title")
+title = page_soup.find("title")  # Searching for the first <title> tag in the HTML document and storing it in a variable called title
